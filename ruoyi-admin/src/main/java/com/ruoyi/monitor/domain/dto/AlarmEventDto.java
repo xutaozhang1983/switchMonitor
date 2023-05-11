0ec1298f4c9c12544c4356a319d08308bebfdf35
leezhua17 <892100089@qq.com>
2023-05-11 16:56:18 +0800

    alarm

--- ruoyi-admin/src/main/java/com/ruoyi/monitor/domain/dto/AlarmEventDto.java
+++ ruoyi-admin/src/main/java/com/ruoyi/monitor/domain/dto/AlarmEventDto.java
@@ -1,10 +1,17 @@
 package com.ruoyi.monitor.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.monitor.domain.TbAlarmEvent;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class AlarmEventDto extends TbAlarmEvent {
     private Long deviceId;
     private Long itemId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
 }
