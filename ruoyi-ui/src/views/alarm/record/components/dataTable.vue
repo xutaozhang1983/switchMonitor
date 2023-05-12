599b33eae3d7647b95787a14559da8385833b1cb
vincent <vincent@192.168.10.103>
2023-05-12 12:49:34 +0800

    update

--- ruoyi-ui/src/views/alarm/record/components/dataTable.vue
+++ ruoyi-ui/src/views/alarm/record/components/dataTable.vue
@@ -7,14 +7,8 @@
     <el-table-column label="接收地址" prop="receiveAddr"/>
     <el-table-column label="发送状态">
       <template #default="scope">
-        <div class="status" v-if="scope.row.status === '0'">
-          <el-icon class="mr5" color="#67C23A" size="20px"><WarningFilled /></el-icon>
-          <span>成功</span>
-        </div>
-        <div class="status" v-else>
-          <el-icon class="mr5" color="#F56C6C" size="20px"><WarningFilled /></el-icon>
-          <span>失败</span>
-        </div>
+        <div class="status status-open" v-if="scope.row.status === '0'">成功</div>
+        <div class="status status-close" v-else>失败</div>
       </template>
     </el-table-column>  
   </el-table>
@@ -38,6 +32,19 @@
 <style lang="scss" scoped>
   .status {
     display: flex;
+    justify-content: center;
     align-items: center;
+    font-size: 12px;
+    width: 50px;
+    padding: 1px 0;
+    border-radius: 5px;
+    &-open {
+      color: #67C23A;
+      border: 1px solid #67C23A;
+    }
+    &-close {
+      color: #F56C6C;
+      border: 1px solid #F56C6C;
+    }
   }
 </style>
